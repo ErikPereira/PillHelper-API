5ee4649c0b0f2e4c36536bf385035626a6d20151
“ErikBezerra” <erik.bezerra@icarotech.com>
2021-07-19 20:21:12 -0300

    IMG-19_07: nova tentativa, porem, sem sucesso

--- src/Python/textRecognizer.py
+++ src/Python/textRecognizer.py
@@ -1,47 +1,33 @@
-# import the necessary packages
+import sys
+import easyocr
 from easyocr import Reader
-import argparse
 import cv2
+
+def ocr(image):
+  reader = easyocr.Reader(['pt','en'])
+  results = reader.readtext(image)
+  return results
 
 def cleanup_text(text):
 	# strip out non-ASCII text so we can draw the text on the image
 	# using OpenCV
 	return "".join([c if ord(c) < 128 else "" for c in text]).strip()
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,	help="path to input image to be OCR'd")
-ap.add_argument("-l", "--langs", type=str, default="en", help="comma separated list of languages to OCR")
-ap.add_argument("-g", "--gpu", type=int, default=-1, help="whether or not GPU should be used")
-args = vars(ap.parse_args())
+def rotate_image(img):
+  # get image height, width
+  (h, w) = img.shape[:2]
+  # calculate the center of the image
+  center = (w / 2, h / 2)
+  angle90 = 90
+  scale = 1.0
+  # Perform the counter clockwise rotation holding at the center
+  # 90 degrees
+  M = cv2.getRotationMatrix2D(center, angle90, scale)
+  rotated90 = cv2.warpAffine(img, M, (h, w))
+  return rotated90
 
-# break the input languages into a comma separated list
-langs = args["langs"].split(",")
-print("[INFO] OCR'ing with the following languages: {}".format(langs))
-# load the input image from disk
-image = cv2.imread(args["image"])
-# OCR the input image using EasyOCR
-print("[INFO] OCR'ing input image...")
-reader = Reader(langs, gpu=args["gpu"] > 0)
-print("[teste] Image: ")
-print(image)
-results = reader.readtext(image)
+all_results=[]
+image = cv2.imread('C:/Users/ErickBezerra/Desktop/ErikBezerra/TCC/PillHelper-API/src/Python/pare.jpg')
+results = ocr(image)
 
-# loop over the results
-for (bbox, text, prob) in results:
-	# display the OCR'd text and associated probability
-	print("[INFO] {:.4f}: {}".format(prob, text))
-	# unpack the bounding box
-	(tl, tr, br, bl) = bbox
-	tl = (int(tl[0]), int(tl[1]))
-	tr = (int(tr[0]), int(tr[1]))
-	br = (int(br[0]), int(br[1]))
-	bl = (int(bl[0]), int(bl[1]))
-	# cleanup the text and draw the box surrounding the text along
-	# with the OCR'd text itself
-	text = cleanup_text(text)
-	cv2.rectangle(image, tl, br, (0, 255, 0), 2)
-	cv2.putText(image, text, (tl[0], tl[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+print(results)
