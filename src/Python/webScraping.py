aab85bb54df71dd42de255ac24fc590d9bea2187
ErikPereira <erik_bezerra_silva@hotmail.com>
2021-07-24 12:12:38 -0300

    IMG-24_07: webSraping done

--- src/Python/webScraping.py
+++ src/Python/webScraping.py
@@ -5,8 +5,53 @@ from bs4 import BeautifulSoup
 def getInformationMedicine(urlMedicine):
     pageMedicine = urllib.request.urlopen(urlMedicine)
     soupMedicine = BeautifulSoup(pageMedicine, 'html5lib')
-    listTitle = soupMedicine.find_all('h2')
-    print(listTitle)
+    webValues = soupMedicine.find('div', attrs={'id': 'innerBula'})
+    information = []
+    bulla = {
+        "title": "",
+        "description": "",
+        "information": ""
+    }
+    for tags in webValues.contents[1].contents:
+        
+        if tags == ' ' and bulla["title"] != "":
+            information.append(bulla)
+            bulla = {
+                "title": "",
+                "description": "",
+                "information": ""
+            }
+
+        elif tags.name == "h2":
+            bulla["title"] = tags.text
+
+        elif tags.name == "h3":
+            bulla["description"] = tags.text
+        
+        elif tags.name == "h4":
+            if tags.text == "Composição":
+                bulla["title"] = tags.text
+                bulla["description"] = "O que contém cada cápsula?"
+            elif tags.text == "Laboratório":
+                bulla["title"] = tags.text
+                bulla["description"] = "Quem o fabrica?"
+            else:
+                bulla["title"] = "Curiosidades"
+                bulla["description"] = tags.text
+
+        elif tags.name == "p":
+            info = re.sub("strong>", "b>", str(tags))
+            info = re.sub("<p>", "", info)
+            info = re.sub("</p>", "", info)
+            bulla["information"] = bulla["information"] + info + " <br> "
+        
+        elif tags.name == "ul":
+            info = re.sub("strong>", "b>", str(tags))
+            bulla["information"] = bulla["information"] + info + " <br> "
+        
+
+    information.append(bulla)
+    return information
 
 
 urlAll = 'https://www.bulario.com/alfa/todos'
@@ -16,11 +61,16 @@ soup = BeautifulSoup(page, 'html5lib')
 listMedicine = soup.find_all('strong')
 listBulla = []
 
-for i in range(0,30):
+for list in listMedicine:
+    if list.contents[0].name != "a":
+        continue
+    
     medicine = {}
-    medicine["name"] = re.sub("^ ", "", listMedicine[i].text)
-    urlMedicine = "https://www.bulario.com" + listMedicine[i].contents[0].attrs['href']
-    medicine["information"] = getInformationMedicine(urlMedicine)
-    print(medicine)
+    medicine["name"] = re.sub("^ ", "", list.text)
 
-print(listMedicine)
+    urlMedicine = "https://www.bulario.com" + list.contents[0].attrs['href']
+    medicine["information"] = getInformationMedicine(urlMedicine)
+    listBulla.append(medicine)
+    # print(medicine)
+
+print(listBulla)
